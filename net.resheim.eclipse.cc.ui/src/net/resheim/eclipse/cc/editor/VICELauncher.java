ce97d411c57a7be856c15f54b21b6a90024784f8
Torkild U. Resheim <tur@itema.no>
2024-01-21 09:45:35 +0100

    Read Vice ini-file when starting

--- net.resheim.eclipse.cc.ui/src/net/resheim/eclipse/cc/editor/VICELauncher.java
+++ net.resheim.eclipse.cc.ui/src/net/resheim/eclipse/cc/editor/VICELauncher.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2024 Torkild Ulvøy Resheim.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * Torkild Ulvøy Resheim <torkildr@gmail.com> - initial API and implementation
+ */
 package net.resheim.eclipse.cc.editor;
 
 import java.lang.ProcessBuilder.Redirect;
@@ -11,7 +22,7 @@ public class VICELauncher implements IEditorLauncher {
 
 	private IPath findViceConfig(IPath file) {
 		IPath folder = file.removeLastSegments(1);
-		IPath config = folder.append("vice.config");
+		IPath config = folder.append("vice.ini");
 		if (config.toFile().exists()) {
 			return config;
 		}
@@ -26,7 +37,7 @@ public class VICELauncher implements IEditorLauncher {
 	public void open(IPath file) {
 		try {
 			List<String> args = new ArrayList<String>();
-			// The file can be anywhere in the file system so we cannot rely
+			// The PRG-file can be anywhere in the file system so we cannot rely
 			// on IWorkspace and related types. So we're assuming that there is
 			// a viceconfig somewhere in the file's folder or in one of the
 			// parent folders.
