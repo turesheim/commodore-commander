b38716b78a6728aa96513b1f4b932ef7ad69534c
Torkild U. Resheim <tur@itema.no>
2024-07-04 20:33:32 +0200

    Clean up

--- net.resheim.eclipse.cc.ui/src/net/resheim/eclipse/cc/builder/KickAssemblerProjectParser.java
+++ net.resheim.eclipse.cc.ui/src/net/resheim/eclipse/cc/builder/KickAssemblerProjectParser.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2024 Torkild Ulvøy Resheim.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * Torkild Ulvøy Resheim <torkildr@gmail.com> - initial API and implementation
+ */
 package net.resheim.eclipse.cc.builder;
 
 import java.io.File;
@@ -11,21 +22,21 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
 
-import net.resheim.eclipse.cc.kickassembler.parser.AbstractKickAssemblerListener;
+import net.resheim.eclipse.cc.kickassembler.parser.KickAssemblerBaseListener;
 import net.resheim.eclipse.cc.kickassembler.parser.KickAssemblerLexer;
 import net.resheim.eclipse.cc.kickassembler.parser.KickAssemblerParser;
-import net.resheim.eclipse.cc.kickassembler.parser.KickAssemblerParser.Import_codeContext;
 
 /**
  * Utilises the {@link KickAssemblerParser} to find import statements and
- * creates a list of these.
+ * creates an ordered list of these while also parsing the imported files in
+ * order to create a tree.
  */
 public class KickAssemblerProjectParser {
 
-	class ImportParser extends AbstractKickAssemblerListener {
+	class ImportParser extends KickAssemblerBaseListener {
 
 		@Override
-		public void exitImport_code(Import_codeContext ctx) {
+		public void exitImport_code(KickAssemblerParser.Import_codeContext ctx) {
 			String fileName = ctx.fileName.getText().replaceAll("\"", "");
 			IResource resource = getRoot().getResource().getParent().findMember(fileName);
 			AssemblyFile file = new AssemblyFile(resource, getRoot());
