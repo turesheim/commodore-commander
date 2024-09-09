80235d380ab10c2b72a964760ca40ba0d92cf624
Torkild U. Resheim <tur@itema.no>
2024-09-09 20:29:48 +0200

    Add copyright headers

--- net.resheim.eclipse.cc.ui/src/net/resheim/eclipse/cc/launch/VICELaunchConfigurationTabGroup.java
+++ net.resheim.eclipse.cc.ui/src/net/resheim/eclipse/cc/launch/VICELaunchConfigurationTabGroup.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2024 Torkild Ulvøy Resheim.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *
+ *   Torkild Ulvøy Resheim <torkildr@gmail.com> - initial API and implementation
+ */
 package net.resheim.eclipse.cc.launch;
 
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
@@ -8,7 +21,6 @@ import org.eclipse.debug.ui.ILaunchConfigurationTab;
 public class VICELaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
 
 	public VICELaunchConfigurationTabGroup() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
