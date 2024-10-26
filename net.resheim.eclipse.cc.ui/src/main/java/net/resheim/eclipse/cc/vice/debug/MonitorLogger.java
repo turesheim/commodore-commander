4813912557731f0a7ccec17fff771573399182df
Torkild U. Resheim <tur@itema.no>
2024-10-26 16:04:13 +0200

    Make ANSI colouring optional

--- net.resheim.eclipse.cc.ui/src/main/java/net/resheim/eclipse/cc/vice/debug/MonitorLogger.java
+++ net.resheim.eclipse.cc.ui/src/main/java/net/resheim/eclipse/cc/vice/debug/MonitorLogger.java
@@ -8,10 +8,14 @@ public class MonitorLogger {
 	public static final int INPUT = 1;
 	public static final int OUTPUT = 2;
 
+	private static final boolean ansi = true;
+
 	public static final void error(MessageConsoleStream out, int source, String message) {
 		StringBuilder sb = new StringBuilder();
+		if (ansi) {
 			sb.append(ANSIColors.RED_BACKGROUND);
 			sb.append(ANSIColors.WHITE);
+		}
 			switch (source) {
 			case USER:
 				sb.append("=== ");
@@ -27,7 +31,9 @@ public class MonitorLogger {
 			while (sb.length() < 79) {
 				sb.append(" ");
 			}
-			sb.append(ANSIColors.RESET);
+			if (ansi) {
+				sb.append(ANSIColors.RESET);
+			}
 			out.println(sb.toString());
 		}
 
@@ -35,18 +41,24 @@ public class MonitorLogger {
 			StringBuilder sb = new StringBuilder();
 			switch (source) {
 			case USER:
-				sb.append(ANSIColors.BLUE_BACKGROUND);
-				sb.append(ANSIColors.WHITE);
+				if (ansi) {
+					sb.append(ANSIColors.BLUE_BACKGROUND);
+					sb.append(ANSIColors.WHITE);
+				}
 				sb.append("=== ");
 				break;
 			case INPUT:
-				sb.append(ANSIColors.CYAN_BACKGROUND);
-				sb.append(ANSIColors.BLACK);
+				if (ansi) {
+					sb.append(ANSIColors.CYAN_BACKGROUND);
+					sb.append(ANSIColors.BLACK);
+				}
 				sb.append("<<< ");
 				break;
 			case OUTPUT:
-				sb.append(ANSIColors.YELLOW_BACKGROUND);
-				sb.append(ANSIColors.BLACK);
+				if (ansi) {
+					sb.append(ANSIColors.YELLOW_BACKGROUND);
+					sb.append(ANSIColors.BLACK);
+				}
 				sb.append(">>> ");
 				break;
 			}
@@ -54,7 +66,9 @@ public class MonitorLogger {
 			while (sb.length() < 79) {
 				sb.append(" ");
 			}
-			sb.append(ANSIColors.RESET);
+			if (ansi) {
+				sb.append(ANSIColors.RESET);
+			}
 			out.println(sb.toString());
 		}
 
